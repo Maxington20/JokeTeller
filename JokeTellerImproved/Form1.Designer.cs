b789f2d195da4bdc7e6f39da43a17738b5372eac
Max Herrington <max.herrington@gmail.com>
2018-01-09 20:51:16 -0500

    Assignment 1 - completed May 2017

--- JokeTellerImproved/Form1.Designer.cs
+++ JokeTellerImproved/Form1.Designer.cs
@@ -28,12 +28,110 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.lblProgramTitle = new System.Windows.Forms.Label();
+			this.btnExit = new System.Windows.Forms.Button();
+			this.btnReset = new System.Windows.Forms.Button();
+			this.btnSetup = new System.Windows.Forms.Button();
+			this.btnPunchline = new System.Windows.Forms.Button();
+			this.txtJoke = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// lblProgramTitle
+			// 
+			this.lblProgramTitle.AutoSize = true;
+			this.lblProgramTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblProgramTitle.Location = new System.Drawing.Point(110, 21);
+			this.lblProgramTitle.Name = "lblProgramTitle";
+			this.lblProgramTitle.Size = new System.Drawing.Size(157, 24);
+			this.lblProgramTitle.TabIndex = 0;
+			this.lblProgramTitle.Text = "The Joke Teller";
+			// 
+			// btnExit
+			// 
+			this.btnExit.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnExit.Location = new System.Drawing.Point(55, 276);
+			this.btnExit.Name = "btnExit";
+			this.btnExit.Size = new System.Drawing.Size(75, 23);
+			this.btnExit.TabIndex = 1;
+			this.btnExit.Text = "Exit";
+			this.btnExit.UseVisualStyleBackColor = true;
+			this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+			// 
+			// btnReset
+			// 
+			this.btnReset.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnReset.Location = new System.Drawing.Point(253, 276);
+			this.btnReset.Name = "btnReset";
+			this.btnReset.Size = new System.Drawing.Size(75, 23);
+			this.btnReset.TabIndex = 2;
+			this.btnReset.Text = "Reset";
+			this.btnReset.UseVisualStyleBackColor = true;
+			this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+			// 
+			// btnSetup
+			// 
+			this.btnSetup.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnSetup.Location = new System.Drawing.Point(83, 189);
+			this.btnSetup.Name = "btnSetup";
+			this.btnSetup.Size = new System.Drawing.Size(75, 23);
+			this.btnSetup.TabIndex = 3;
+			this.btnSetup.Text = "Setup";
+			this.btnSetup.UseVisualStyleBackColor = true;
+			this.btnSetup.Click += new System.EventHandler(this.btnSetup_Click);
+			// 
+			// btnPunchline
+			// 
+			this.btnPunchline.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.btnPunchline.Enabled = false;
+			this.btnPunchline.Location = new System.Drawing.Point(210, 189);
+			this.btnPunchline.Name = "btnPunchline";
+			this.btnPunchline.Size = new System.Drawing.Size(75, 23);
+			this.btnPunchline.TabIndex = 4;
+			this.btnPunchline.Text = "Punchline";
+			this.btnPunchline.UseVisualStyleBackColor = true;
+			this.btnPunchline.Click += new System.EventHandler(this.btnPunchline_Click);
+			// 
+			// txtJoke
+			// 
+			this.txtJoke.BackColor = System.Drawing.Color.LightSteelBlue;
+			this.txtJoke.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.txtJoke.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.txtJoke.Location = new System.Drawing.Point(55, 103);
+			this.txtJoke.Multiline = true;
+			this.txtJoke.Name = "txtJoke";
+			this.txtJoke.ReadOnly = true;
+			this.txtJoke.Size = new System.Drawing.Size(273, 39);
+			this.txtJoke.TabIndex = 5;
+			this.txtJoke.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.LightSteelBlue;
+			this.ClientSize = new System.Drawing.Size(378, 325);
+			this.Controls.Add(this.txtJoke);
+			this.Controls.Add(this.btnPunchline);
+			this.Controls.Add(this.btnSetup);
+			this.Controls.Add(this.btnReset);
+			this.Controls.Add(this.btnExit);
+			this.Controls.Add(this.lblProgramTitle);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label lblProgramTitle;
+		private System.Windows.Forms.Button btnExit;
+		private System.Windows.Forms.Button btnReset;
+		private System.Windows.Forms.Button btnSetup;
+		private System.Windows.Forms.Button btnPunchline;
+		private System.Windows.Forms.TextBox txtJoke;
 	}
 }
 
--- JokeTellerImproved/Form1.cs
+++ JokeTellerImproved/Form1.cs
@@ -16,5 +16,85 @@ namespace JokeTellerImproved
 		{
 			InitializeComponent();
 		}
+
+		public string RandomSetup()
+		{
+			Random rnd = new Random();
+			int jokeNumber = rnd.Next(1, 6);
+			string setup = "";
+
+			switch (jokeNumber)
+			{
+				case 1:
+					setup = "Why did the Chicken cross the road?";
+					break;
+				case 2:
+					setup = "Who shot the sherrif?";
+					break;
+				case 3:
+					setup = "I almost had a 3some last night.";
+					break;
+				case 4:
+					setup = "What's black, white, and read all over?";
+					break;
+				default:
+					setup = "Where am I?";
+					break;
+			}
+			return txtJoke.Text= setup;
+		}
+
+		public string Punchline(string setup)
+		{
+			setup = txtJoke.Text;
+			string punchLine = "";
+
+			switch (setup)
+			{
+				case "Why did the Chicken cross the road?":
+					punchLine = "To get to the other side!";
+					break;
+				case "Who shot the sherrif?":
+					punchLine = "Certainly not Bob Marley!";
+					break;
+				case "I almost had a 3some last night.":
+					punchLine = "I just needed two more people!";
+					break;
+				case "What's black, white, and read all over?":
+					punchLine = "A newspaper!";
+					break;
+				default:
+					punchLine = "How the hell should I know?";
+					break;
+			}
+			return txtJoke.Text = punchLine;
+		}
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnSetup_Click(object sender, EventArgs e)
+		{
+			RandomSetup();
+			btnPunchline.Enabled = true;
+		}
+
+		private void btnPunchline_Click(object sender, EventArgs e)
+		{
+			Punchline(txtJoke.Text);
+			btnPunchline.Enabled = false;
+		}
+
+		private void btnExit_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+
+		private void btnReset_Click(object sender, EventArgs e)
+		{
+			txtJoke.Text = "";
+			btnPunchline.Enabled = false;
+		}
 	}
 }
